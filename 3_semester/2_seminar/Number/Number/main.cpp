372f8c60ecad3376decf9134c332112e2d150843
FokySN1K <yaroslav@stetsenko.su>
2022-09-21 17:28:39 +0300

    Последние изменения

--- 3_semester/2_seminar/Number/Number/main.cpp
+++ 3_semester/2_seminar/Number/Number/main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 void fibonachi(int n);
 void factorial(int n);
 void num_grad(long long n);
+void num_grad(char* s);
 
 int main()
 {
@@ -17,9 +18,10 @@ int main()
     s[0] = 49;
     cout << (int)s[0];
    */
-    Number a = "11114", b = "256";
-    factorial(1000);
-    num_grad(4761963248413673697);
+    //Number a = "11114", b = "256";
+    //factorial(1000);
+    char s[] = "90560792656972947582439785608972465789628974587264056284658721771";
+    num_grad(s);
 }
 
 
@@ -81,4 +83,24 @@ void num_grad(long long n) {
         }
     }
     cout << "n = " << n << " steps = " << steps << " max = " << max << endl;
+}
+void num_grad(char* s) {
+    Number x = s;
+    Number unit = 1;
+    Number third = 3;
+    long long steps = 0;
+    Number max = x;
+    while (x.get_size() != 1 || (int)x[0] != 1) {
+        steps++;
+        if (isEven(x)) {
+            x.divide_by_2();
+        }
+        else {
+            x = third * x + unit;
+        }
+        if (max < x) {
+            max = x;
+        }
+    }
+    cout << "n = " << s << " steps = " << steps << " max = " << max << endl;
 }
